2d64169de6cd5ad589e7dee2bf7ab8cc3ec87fea
DuncantheeDuncan <mthembup69@gmail.com>
2019-06-25 12:24:26 +0200

    initial commit

---
+++ action_page_from_serch_button.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+
+<?php 
+
+echo "this is PHP..";
+ ?>
+<h1>what ever that been serched it will apper on this page..</h1>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,17 @@
+var days = new Date();
+
+//to write the date
+function weekDay() {
+    var today = days.getUTCDay();
+    var month = days.getUTCMonth();
+    var daysOfTheWeek = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saterday");
+    var monthOfTheYear = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+
+    document.getElementById("day").innerHTML = daysOfTheWeek[today]+" "+days.getDate()+" "+monthOfTheYear[month]; 
+}
+
+//to open a new website 
+function toRegister() {
+    window.open("InclassStudents.html");
+
+}
