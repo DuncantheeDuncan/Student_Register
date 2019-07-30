feedf912e38e55fd9de38bc371ec4f2f2345bc78
DuncantheeDuncan <mthembup69@gmail.com>
2019-07-30 08:19:20 +0200

    adding modules

---
+++ javascript/admin/add-student-lecture.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//     $('.radioshow').on('change', function() { 
+//         var val = $(this).attr('data-class'); 
+//         $('.allshow').hide();
+//         $('.' + val).show();        
+//     });
+// }); 
+
+// alert("connected");
