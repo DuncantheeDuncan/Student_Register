f71d7ff35ad75c55339cde04ab68f56794239488
DuncantheeDuncan <mthembup69@gmail.com>
2019-11-01 18:20:39 +0200

    fixing the screen sizes

--- javascript/indexjs.js
+++ javascript/indexjs.js
@@ -1,28 +1,10 @@
-// alert("Indicating that javascript is still connected..");
 
 
-    // validating that only  numbers are entered!!
+// pblocking all keys pressed except for the numbers!
     function isNumber(evt) {
         var iKeyCode = (evt.which) ? evt.which : evt.keyCode
         if (iKeyCode != 10 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
             return false;
 
         return true;
-    }    
-
-function pwarnin(){
-						Swal.fire({
-						
-					  type: 'error',
-					  title: 'Oops...',
-					  text: 'Something went wrong!',
-					  timer: 1800000,
-					  footer: '<a href>Why do I have this issue?</a>'
-					})
-alert("aibo!!");
-		
-
-
-					}
-
-					
+}
