8078c94958daa59bb0101b69886526132e51db4e
DuncantheeDuncan <mthembup69@gmail.com>
2019-11-04 16:11:24 +0200

    setting files

---
+++ client/other/j.php
@@ -0,0 +1,9 @@
+<?php
+
+// if(!isset($_SESSION['loggedin'])){
+// 	echo "connected confirmation";
+// 	header('location: ../index.php');
+// 	exit();
+// }
+
+?>
---
+++ server/index.js
@@ -0,0 +1,31 @@
+let express = require('express');
+let app = express();
+
+//app.get("/", function(req, res){
+  //res.send("student register sever on");
+//});
+
+let PORT = process.env.PORT || 3008;
+
+app.listen(PORT, function(){
+  console.log('App starting on port http://localhost:', PORT);
+});
+
+
+const exphbs  = require('express-handlebars');
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+
+
+app.get('/admin', function (req, res) {
+  console.log("Hello");
+  res.render('index');
+});
