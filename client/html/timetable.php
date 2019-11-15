1168ebfbb8aaab46c129f6b08e73a29b26416e8a
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2019-11-15 12:48:27 +0200

    p

---
+++ client/html/timetable.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Timetable</title>
+	<link rel="stylesheet" href="../bootstrap/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../css/timetable.css">
+	<script src="../jquery-1.3.2.min.js/jquery-1.3.2.min.js"></script>
+	<script src="../javascript/datesForTimeable.js"></script>
+	<script src="../javascript/timetable.js"></script>
+</head>
+
+<body>
+	<div class="container">
+		<div id="logo_container">
+			<img class="img-responsive" id="Logo" src="../images/home_rgi_logo.png">
+			Time:<label id="displaytimerr"> </label>
+		</div>
+		<div class="main_div"><br>
+			<p id="day">
+				<ul class="list-group" id="myList">
+					<li id="active1" class="list-group-item"><a href="Student.html">Networks</a></li>
+					<li id="active2" class="list-group-item"><a href="Student.html"></a></li>
+					<li id="active3" class="list-group-item"><a href="Student.html">3 Break</a></li>
+					<li id="active4" class="list-group-item"><a href="Student.html">4 LECTURE</a></li>
+					<li id="active5" class="list-group-item"><a href="Student.html">5 LECTURE</a></li>
+					<li id="active6" class="list-group-item"><a href="Student.html">6 Break</a></li>
+					<li id="active7" class="list-group-item"><a href="Student.html">7 LECTURE</a></li>
+					<li id="active8" class="list-group-item"><a href="Student.html">8 LECTURE</a></li>
+					<li id="active9" class="list-group-item"><a href="Student.html">9 LECTURE</a></li>
+				</ul>
+		</div>
+		<div id="backlink">
+			<a id="lOGOUT" href="../index.html"><label>LOGOUT</label></a>
+		</div>
+	</div>
+</body>
+
+</html>
---
+++ client/php/timetable.php
@@ -0,0 +1,46 @@
+<
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Timetable</title>
+	<link rel="stylesheet" href="../bootstrap/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../css/timetable.css">
+	<script src="../jquery-1.3.2.min.js/jquery-1.3.2.min.js"></script>
+	<script src="../javascript/datesForTimeable.js"></script>
+	<script src="../javascript/timetable.js"></script>
+</head>
+
+<body>
+	<div class="container">
+		<div id="logo_container">
+			<img class="img-responsive" id="Logo" src="../images/home_rgi_logo.png">
+			Time:<label id="displaytimerr"> </label>
+		</div>
+		<div class="main_div"><br>
+			<p id="day">
+
+
+
+				<ul class="list-group" id="myList">
+					<li id="active1" class="list-group-item"><a href="Student.php">1 LECTURE</a></li>
+					<li id="active2" class="list-group-item"><a href="Student.php">2 LECTURE</a></li>
+					<li id="active3" class="list-group-item"><a href="Student.php">3 Break</a></li>
+					<li id="active4" class="list-group-item"><a href="Student.php">4 LECTURE</a></li>
+					<li id="active5" class="list-group-item"><a href="Student.php">5 LECTURE</a></li>
+					<li id="active6" class="list-group-item"><a href="Student.php">6 Break</a></li>
+					<li id="active7" class="list-group-item"><a href="Student.php">7 LECTURE</a></li>
+					<li id="active8" class="list-group-item"><a href="Student.php">8 LECTURE</a></li>
+					<li id="active9" class="list-group-item"><a href="Student.php">9 LECTURE</a></li>
+				</ul>
+		</div>
+		<div id="backlink">
+			<a id="lOGOUT" href="../index.php"><label>LOGOUT</label></a>
+		</div>
+	</div>
+</body>
+
+</html>
