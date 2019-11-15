56ad36e01d9144d1a8e6305651774f36fb4d327d
DuncantheeDuncan <mthembup69@gmail.com>
2019-11-15 03:00:07 +0200

    pushing for arnorld

--- client/index.php
+++ client/index.php
@@ -3,76 +3,72 @@
 // require_once "sessions_start.php";
 
  ?>
- 
-<html>
-<head>
-	<title>Log-in page</title>
+ <!DOCTYPE html>
+ <html>
 
-	<script type="text/javascript" src="jquery-1.3.2.min.js/jquery-1.3.2.min.js"></script>
+ <head>
+     <meta charset='utf-8'>
+     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+     <title>LOGIN PAGE</title>
+     <meta name='viewport' content='width=device-width, initial-scale=1'>
 
-	<script typetext="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-
-	<script typetext="text/javascript" src="javascript/indexjs.js"></script>
-
-
-	<link rel="stylesheet" type="text/css" href= "css/main.css">
-	<script src="sweetalert2.all.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+     <link rel="stylesheet" type="text/css" href="css/index.css">
 
 
-</head>
-<body>
- 
-	<div class="container">
-		<div id="logo_container">
-			<img class="img-responsive" id="Logo" src="images/home_rgi_logo.png">
-		</div>
-		<div class= "main_div">
-				<h2>LECTURE<br> SIGN IN</h2>
-				<p>ENTER YOUR ASSIGNED CODE</p><BR>
-				<!-- <?php	echo $_SESSION['name']; ?> -->
-		
+ </head>
+ <body>
 
-			<form class="input_group"  id="form" method="post" action=""  >
-				<input id="input_box"
-				 type="numbers" name="passcode"
-				  placeholder="unique code"
-				  onkeypress="javascript:return isNumber(event)"  
-				  title="enter the assigned code">
-				  <p id="one"></p>
-				<!-- <input type="submit" name="submit"  value="Submit" class="btnSubmit"> -->
-			</form>
-				
-	
-			<script type="text/javascript">
-				 //if numbers enterd are equal to 10 digits it must attempt to load to the next page..
+ <div class="container">
+     <div id="logo_container">
+         <img class="img-responsive" id="Logo" src="images/home_rgi_logo.png">
+         <!-- <img class="img-responsive" id="Logo" src="confirmation.html"> -->
+         <script type="text/javascript" src="jquery-1.3.2.min.js/jquery-1.3.2.min.js"></script>
+         <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+     </div>
+     <div class="main_div">
+         <h2>STAFF SIGN IN</h2>
+         <p>ENTER YOUR ASSIGNED CODE</p><BR>
+         <form class="input_group" id="form" method="post" action="">
+             <input id="input_box" type="numbers" name="passcode" placeholder="unique code"
+                    onkeypress="javascript:return isNumber(event)">
 
-				$('input').keypress(function () {
-					var ten = 10;
-					var login = 1111111111;// this is temporary
-					$('p').text(ten.size);
+         </form>
+         <script type="text/javascript">
+ 				//if numbers entered are equal to 10 digits it must attempt to load to the next page..
 
-					if (this.value.length == ten) {$(function(){ $("#form").submit();})
-					if(this.value == login){// this if block is temp.
-				// window.close("index.php");
-				window.open("php/confirmation.php");
-				}else {
+ 				$('input').keypress(function () {
+ 					var ten = 10;
+ 					var Lecture_login = 1111111111; // this is temporary 'Lecturer'
+ 					var Admin_login = 5555555555; // this is temp 'admin'
+ 					$('p').text(ten.size);
 
-					// alert("unrecognised code");
-					document.getElementById("one").innerHTML =  warnin();
-					
+ 					if (this.value.length == ten) {
+ 						$(function () {
+ 							$("#form").submit();
+ 						});
+ 						if (this.value == Lecture_login) { // this if block is temp.
 
-				}
-				
-				  
-					}
-				});
-			</script>
+ <!--    window.location.assign("html/confirmation.html");-->
 
+ window.location.replace("html/confirmation.html");
 
+ 						} else if (this.value == Admin_login) {
 
-		</div>
-	</div>
-</body>
-</html>
+ window.location.replace("html/admin/administrator.html");
+
+ 						} else {
+ 							alert("Incorrect login ID");
+
+ 						}
+ 					}
+ 				});
+
+         </script>
+     </div>
+ </div>
+
+ <script type="text/javascript" src="javascript/indexjs.js"></script>
+
+ <script src='main.js'></script>
+ </body>
+ </html>
